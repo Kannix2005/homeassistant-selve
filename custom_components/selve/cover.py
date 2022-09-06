f51c06a27d54c8b06f681bd238f6874c8d06ed59
Kannix2005 <stefan.altheimer@googlemail.com>
2022-09-06 17:20:35 +0200

    fixed cover.py

--- custom_components/selve/cover.py
+++ custom_components/selve/cover.py
@@ -4,9 +4,11 @@ Support for Selve cover - shutters etc.
 from .const import DOMAIN
 from homeassistant.core import callback
 import logging
+from selve import Gateway
 
 import voluptuous as vol
 
+from homeassistant.const import CONF_PORT
 from homeassistant.components.cover import (
     CoverEntity,
     ATTR_POSITION,
@@ -70,6 +72,15 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     config = hass.data[DOMAIN][config_entry.entry_id]
+
+    serial_port = config[CONF_PORT]
+    try:
+        selve = Gateway(serial_port, False)
+    except:
+        _LOGGER.exception("Error when trying to connect to the selve gateway")
+        return False
+    config["controller"] = selve
+
     await async_setup_platform(hass, config, async_add_entities)
 
 class SelveCover(SelveDevice, CoverEntity):

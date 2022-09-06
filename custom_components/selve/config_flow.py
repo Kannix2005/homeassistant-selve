ff4b9eeac8cffcdfa10955d79ac8e687af676a88
Kannix2005 <stefan.altheimer@googlemail.com>
2022-09-06 13:01:23 +0200

    Changed config flow

--- custom_components/selve/config_flow.py
+++ custom_components/selve/config_flow.py
@@ -60,38 +60,60 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
+    async def async_step_import(self, device_config):
+        """Import a configuration.yaml config, if any."""
+        try:
+            await validate_input(self.hass, device_config)
+        except AlreadyConfigured:
+            return self.async_abort(reason="already_configured")
+
+        port = device_config[CONF_PORT]
+        return self.async_create_entry(
+            title=self.domain,
+            data=device_config
+        )
+
+    
+    async def async_step_user(self, user_input=None):
+        """Give initial instructions for setup."""
+        if user_input is not None:
+            return await self.async_step_initial_options()
+
+        return self.async_show_form(step_id="user")
+
+    async def async_step_initial_options(
+        self, user_input = None
     ) -> FlowResult:
         """Handle the initial step."""
-        if user_input is None:
-            return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
-            )
-        
         errors = {}
 
-        try:
-            info = await validate_input(self.hass, user_input)
-        except GatewayNotReadyError:
-            _LOGGER.exception("gateway not ready")
-            errors["base"] = "gateway_not_ready"
-        except ConnectionError:
-            errors["base"] = "invalid_port"
-        except Exception:  # pylint: disable=broad-except
-            _LOGGER.exception("Unexpected exception")
-            errors["base"] = "unknown"
-        else:
-            return self.async_create_entry(title=self.domain, data=info)
+        if user_input is not None:
+        
+            try:
+                info = await validate_input(self.hass, user_input)
+                return self.async_create_entry(title=self.domain, data=user_input)
+            except GatewayNotReadyError:
+                _LOGGER.exception("gateway not ready")
+                errors["base"] = "gateway_not_ready"
+            except ConnectionError:
+                errors["base"] = "invalid_port"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            except AlreadyConfigured:
+                return self.async_abort(reason="already_configured")
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="initial_options", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
 
 
+
+class AlreadyConfigured(HomeAssistantError):
+    """Error to indicate this device is already configured."""
+
 class GatewayNotReadyError(HomeAssistantError):
     """Error to indicate we cannot connect."""
-
 
 class ConnectionError(HomeAssistantError):
     """Error to indicate there is invalid auth."""

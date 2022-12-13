441f189e7fe03fefe44578e14be14bd16b915996
Kannix2005 <stefan.altheimer@googlemail.com>
2022-12-13 17:32:00 +0100

    Fix

--- custom_components/selve/cover.py
+++ custom_components/selve/cover.py
@@ -165,7 +165,7 @@ class SelveCover(SelveDevice, CoverEntity):
     @property
     def device_info(self) -> DeviceInfo:
         """Return the device info."""
-        fwV = self.controller.getGatewayFirmwareVersion()
+        fwV = 0 #self.controller.getGatewayFirmwareVersion()
         gId = self.controller.getGatewaySerial()
         return DeviceInfo(
             identifiers={
